6aec01496f70918e756b0b8b9559e0b1ac8bf5d9
Philip C <philip.cressler@gmail.com>
2015-12-22 20:11:48 -0700

    new tileset

--- js/screens/play.js
+++ js/screens/play.js
@@ -4,7 +4,7 @@ game.PlayScreen = me.ScreenObject.extend({
      */
     onResetEvent: function() {
         // load level
-        me.levelDirector.loadLevel("test");
+        me.levelDirector.loadLevel("main_tilemap");
         
         // reset the score
         game.data.score = 0;
