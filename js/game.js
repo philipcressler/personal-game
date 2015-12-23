f43c41ff5eb0aab98af24016435f8b73da8093af
Philip C <philip.cressler@gmail.com>
2015-12-22 21:21:53 -0700

    add play/blog button

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(640, 480, {wrapper : "screen", scale : "2"})) {
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto", scaleMethod: "stretch"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
